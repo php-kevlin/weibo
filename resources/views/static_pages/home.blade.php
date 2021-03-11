b40d8550b096ba415adc48e537b33b9bc945ed51
php-kevlin <2426609750@qq.com>
2021-03-11 17:43:02 +0800

    基础页面

---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title',"首页")
+@section('content')
+  首页view
+@stop
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');

ba399ca4f97bf4f185d05760f51b3d42549f946a
php-kevlin <2426609750@qq.com>
2021-03-19 17:14:14 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
+Route::get('signup', 'UserController@create')->name('signup');
+

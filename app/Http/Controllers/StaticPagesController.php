241b786585c3251cbce7feca6417038d6795b02d
php-kevlin <2426609750@qq.com>
2021-04-08 11:16:46 +0800

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StaticPagesController extends Controller
 {
     //
     public function home()
     {
-//        echo "hello";die();
-        return view('static_pages/home');
+        $feed_items = [];
+        if (Auth::check()){
+            $feed_items = Auth::user()->feed()->paginate(30);
+        }
+        return view('static_pages/home',compact('feed_items'));
     }
 
     public function help()
--- app/Models/User.php
+++ app/Models/User.php
@@ -63,4 +63,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Status::class);
     }
+
+    public function feed()
+    {
+        return $this->statuses()->orderBy('created_at','desc');
+    }
 }

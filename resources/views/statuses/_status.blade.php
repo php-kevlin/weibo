20e4425bfc52d6a0cc2b1cf94424d652fc4824a6
php-kevlin <2426609750@qq.com>
2021-04-08 11:51:16 +0800

    删除微博

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -6,4 +6,14 @@
     <h5 class="mt-0 mb-1">{{ $user->name }} <small> / {{ $status->created_at->diffForHumans() }}</small></h5>
     {{ $status->content }}
   </div>
+
+  @can('destroy',$status)
+   <form  method="post" action="{{route('statuses.destroy',$status->id)}}" onsubmit="return  confirm('你确定要删除本条微博吗?');">
+     {{ csrf_field() }}
+     {{method_field('DELETE')}}
+     <button type="submit" class="btn btn-sm btn-danger">删除</button>
+   </form>
+  @endcan
+
+
 </li>
--- resources/views/user/_user.blade.php
+++ resources/views/user/_user.blade.php
@@ -9,7 +9,6 @@
     <form action="{{route('user.destroy',$user->id)}}" method="post" class="float-right">
       {{csrf_field()}}
       {{method_field('DELETE')}}
-
       <button type="submit" class="btn btn-sm btn-danger delete-btn">删除</button>
     </form>
   @endcan
